57138c222b39cd07395ab32f59f29d66c0f42df3
EDDY1015 <thakuraditya601@gmail.com>
2024-10-01 23:28:29 -0400

    Add files via upload

---
+++ Lab01/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+
+function MyComponent ()
+{
+const handleClick = () => alert("Hello, React!");
+
+return (
+<div>
+<h1> Hello, React! </h1>
+<button onClick={handleClick}> Click Me </button>
+</div>
+);
+}
+export default MyComponent;
