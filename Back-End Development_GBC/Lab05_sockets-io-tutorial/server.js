0451c7392b8b9105cf2ed98b43b4087a5b22a4d2
EDDY1015 <thakuraditya601@gmail.com>
2024-10-18 15:37:30 -0400

    Add files via upload

---
+++ Back-End Development_GBC/Lab05_sockets-io-tutorial/server.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const http = require ('http');
+const path = require ('path');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = 3000;
+server.listen(port, () => {
+    console.log('Server running at http;//localhost:${port}');
+
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
+
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('Message recieved;', msg);
+        io.emit('chat message', msg);
+
+    });
+
+});
